e59f76a92bda9264923d779ebf09df78d6ea7505
terrenceliu <yl124@rice.edu>
2018-08-14 16:12:03 +0800

    change port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.static(__dirname + '/docs/'));
 
-var port = process.env.PORT || 8080
+var port = process.env.PORT || 8081
 
 app.listen(port, function() {
     console.log('Server listens on port ' + port + '.');
